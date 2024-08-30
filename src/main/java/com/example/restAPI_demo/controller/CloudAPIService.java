3af4269f4043aec946b584b92a0bca4b6a1fe012
PC <bhavyathota72@gmail.com>
2024-08-30 15:04:04 +0530

    ji

---
+++ src/main/java/com/example/restAPI_demo/controller/CloudAPIService.java
@@ -0,0 +1,10 @@
+package com.example.restAPI_demo.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/cloudvendor")
+public class CloudAPIService {
+
+}
